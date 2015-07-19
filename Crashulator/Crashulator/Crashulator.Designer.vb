45c3bff8769a788028027786883730e708d07459
Nanda Kumar Nair <nanda2568@hotmail.com>
2015-07-19 18:06:49 -0500

    First Commit
    A version of calculator

---
+++ Crashulator/Crashulator/Crashulator.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCrashulator
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtResult = New System.Windows.Forms.TextBox
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.txtOperand1 = New System.Windows.Forms.TextBox
+        Me.txtOperand2 = New System.Windows.Forms.TextBox
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.btnExecute = New System.Windows.Forms.Button
+        Me.cmbOperation = New System.Windows.Forms.ComboBox
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.SuspendLayout()
+        '
+        'txtResult
+        '
+        Me.txtResult.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtResult.Location = New System.Drawing.Point(92, 120)
+        Me.txtResult.Name = "txtResult"
+        Me.txtResult.Size = New System.Drawing.Size(100, 29)
+        Me.txtResult.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(10, 124)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(73, 25)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Result"
+        '
+        'txtOperand1
+        '
+        Me.txtOperand1.Location = New System.Drawing.Point(92, 6)
+        Me.txtOperand1.Name = "txtOperand1"
+        Me.txtOperand1.Size = New System.Drawing.Size(100, 20)
+        Me.txtOperand1.TabIndex = 2
+        '
+        'txtOperand2
+        '
+        Me.txtOperand2.Location = New System.Drawing.Point(92, 32)
+        Me.txtOperand2.Name = "txtOperand2"
+        Me.txtOperand2.Size = New System.Drawing.Size(100, 20)
+        Me.txtOperand2.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(57, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Operand 1"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 35)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(57, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Operand 2"
+        '
+        'btnExecute
+        '
+        Me.btnExecute.Location = New System.Drawing.Point(71, 85)
+        Me.btnExecute.Name = "btnExecute"
+        Me.btnExecute.Size = New System.Drawing.Size(121, 23)
+        Me.btnExecute.TabIndex = 11
+        Me.btnExecute.Text = "Execute"
+        Me.btnExecute.UseVisualStyleBackColor = True
+        '
+        'cmbOperation
+        '
+        Me.cmbOperation.FormattingEnabled = True
+        Me.cmbOperation.Items.AddRange(New Object() {"Divide", "Square Root", "Factorial", "Sum Range"})
+        Me.cmbOperation.Location = New System.Drawing.Point(71, 58)
+        Me.cmbOperation.Name = "cmbOperation"
+        Me.cmbOperation.Size = New System.Drawing.Size(121, 21)
+        Me.cmbOperation.TabIndex = 12
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 61)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(53, 13)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "Operation"
+        '
+        'frmCrashulator
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(210, 163)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.cmbOperation)
+        Me.Controls.Add(Me.btnExecute)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtOperand2)
+        Me.Controls.Add(Me.txtOperand1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtResult)
+        Me.Name = "frmCrashulator"
+        Me.Text = "Crashulator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtResult As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtOperand1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtOperand2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents btnExecute As System.Windows.Forms.Button
+    Friend WithEvents cmbOperation As System.Windows.Forms.ComboBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+
+End Class
---
+++ Crashulator/Crashulator/Crashulator.vb
@@ -0,0 +1,137 @@
+﻿Public Class frmCrashulator
+
+    Private Sub btnExecute_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnExecute.Click
+
+        ' We will use this procedure to demonstrate how to catch multiple 
+        ' different types of exceptions
+        Try
+            ' Select case to figure out what operation to actually execute
+            Select Case cmbOperation.SelectedItem.ToString()
+                Case "Square Root"
+                    Dim op1, result As Decimal
+                    op1 = Convert.ToDecimal(txtOperand1.Text)
+                    result = Math.Sqrt(op1)
+                    txtResult.Text = result.ToString()
+                Case "Divide"
+                    Dim result, op1, op2 As Decimal
+                    op1 = Convert.ToDecimal(txtOperand1.Text)
+                    op2 = Convert.ToDecimal(txtOperand2.Text)
+                    result = op1 / op2
+                    txtResult.Text = result.ToString()
+                Case "Factorial"
+                    Dim result As Long
+                    Dim op1 As Long
+                    op1 = Convert.ToInt32(txtOperand1.Text)
+                    result = Factorial(op1)
+                    txtResult.Text = result.ToString()
+                Case "Sum Range"
+                    Dim result As Integer
+                    Dim op1, op2 As Integer
+                    op1 = Convert.ToInt16(txtOperand1.Text)
+                    op2 = Convert.ToInt16(txtOperand2.Text)
+                    result = SumRange(op1, op2)
+                    txtResult.Text = result.ToString()
+                Case Else
+                    Throw New Exception("Unknown operation")
+            End Select
+
+        Catch tmdEx As TooManyDigitsException
+            MessageBox.Show(tmdEx.NumberOfDigits.ToString() & " digits: " & tmdEx.Message)
+
+        Catch nullEx As NullReferenceException
+            MessageBox.Show("You probably forgot to select an operation: " & nullEx.Message)
+
+        Catch divEx As DivideByZeroException
+            MessageBox.Show("Egg under the table: " & divEx.Message)
+
+        Catch ofEx As OverflowException
+            MessageBox.Show("Result won't fit: " & ofEx.Message)
+
+        Catch ex As Exception
+            MessageBox.Show("General Exception: " & ex.Message)
+
+        End Try
+
+    End Sub
+
+    Private Function Factorial(ByVal value As Long) As Long
+        Dim result As Long = 1
+
+        ' For each number between the incoming argument and 1 (step = -1)
+        ' keep multiplying
+        For x As Long = value To 1 Step -1
+            result *= x
+        Next
+
+        ' Here we'll do something a little different with a new exception
+        ' If we have more than 10 digits, it won't look nice in the form's
+        ' text box (I know, that's not a realistic exception, but it's an
+        ' understandable example of how a custom exception can be used)
+        ' ... imagine this is a handheld calculator with a very small LED screen
+        Dim resultString As String = result.ToString()
+        ' check the length of this result - if it's more than 10 characters
+        ' then throw an exceptoin of our custom type
+        If resultString.Length >= 10 Then
+            Dim tmdEx As New TooManyDigitsException("Factorial calculation result contains too many digits to display.")
+            tmdEx.NumberOfDigits = resultString.Length
+            Throw tmdEx
+        End If
+
+        Return result
+
+    End Function
+
+    Private Function SumRange(ByVal lowVal As Integer, ByVal highVal As Integer) As Integer
+        Dim result As Integer = 0
+
+        ' First, check to see if the highVal is actually greater than
+        ' the low val.  If it's not, throw an arithmetic exception.
+        If lowVal > highVal Then
+            Throw New Exception("Low Value must be less than High Value")
+        End If
+
+
+        ' For each number between the incoming arguments, keep adding
+        For x As Integer = lowVal To highVal
+            result += x
+        Next
+
+        Return result
+
+    End Function
+
+
+End Class
+
+' Define a new exception class to handle a specific type of application issue
+' that we want to handle as an exception
+Public Class TooManyDigitsException
+    Inherits System.ApplicationException
+
+    Private _numDigits As Long
+
+    Property NumberOfDigits() As Long
+        Get
+            Return _numDigits
+        End Get
+        Set(ByVal value As Long)
+            _numDigits = value
+        End Set
+    End Property
+
+    Public Sub New()
+    End Sub
+
+    ' Creates a Sub New for the exception that allows you to set the
+    ' message property when thrown.
+    Public Sub New(ByVal Message As String)
+        MyBase.New(Message)
+    End Sub
+    ' Creates a Sub New that can be used when you also want to include
+    ' the inner exception.
+    Public Sub New(ByVal Message As String, ByVal Inner As Exception)
+        MyBase.New(Message)
+    End Sub
+
+
+End Class
